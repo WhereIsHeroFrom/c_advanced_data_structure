19f5b8fadc51c2d4507cf92f86f303b48038b8da
WhereIsHeroFrom <menjitianya2007@163.com>
2026-05-02 08:45:44 +0800

    完善模板

--- 05、单调栈/5-3 刷题实战/5-3-3 最大区间/main.c
+++ 05、单调栈/5-3 刷题实战/5-3-3 最大区间/main.c
@@ -1,40 +1,50 @@
-
 #include <stdio.h>
 
 /////////////////////////////单调栈模板/////////////////////////////
-#define maxn 300001
 #define type int
+#define maxn 300001
 #define inf -2000000000
+typedef int (* cmp_t)(type a, type b);
 
 typedef struct {
     int data[maxn];
     int top;
 } MonotonicStack;
 
-MonotonicStack stk;
 // <  单调递增栈(栈底->栈顶)：inf 1 2 3 4 5
-// <= 单调不减栈(栈底->栈顶)：inf 2 2 3 3 4
-// >  单调递减栈(栈底->栈顶)：inf 6 5 4 3 2
-// >= 单调不增栈(栈底->栈顶): inf 6 6 5 4 3
-int cmp(int a, int b) {
+int cmpLT(type a, type b) {
     return a < b;
+}
+// <= 单调不减栈(栈底->栈顶)：inf 2 2 3 3 4
+int cmpLE(type a, type b) {
+    return a <= b;
+}
+// >  单调递减栈(栈底->栈顶)：inf 6 5 4 3 2
+int cmpGT(type a, type b) {
+    return a > b;
+}
+// >= 单调不增栈(栈底->栈顶): inf 6 6 5 4 3
+int cmpGE(type a, type b) {
+    return a >= b;
 }
 
 // ans[i] 代表从 i 往左找，找到的第一个满足 cmp(h[x], h[i]) 的下标 x
-void findFirstMeetOnLeft(int n, type h[], int ans[]) {
+void findFirstMeetOnLeft(MonotonicStack* stk, 
+    int n, type h[], int ans[], cmp_t cmp) {
     h[0] = inf;
-    stk.top = 0;
-    stk.data[stk.top++] = 0;
+    stk->top = 0;
+    stk->data[stk->top++] = 0;
     for (int i = 1; i <= n; ++i) {
-        while (!cmp(h[stk.data[stk.top - 1]], h[i])) {
-            stk.top--;
+        while (!cmp(h[stk->data[stk->top - 1]], h[i])) {
+            stk->top--;
         }
-        ans[i] = stk.data[stk.top - 1];
-        stk.data[stk.top++] = i;
+        ans[i] = stk->data[stk->top - 1];
+        stk->data[stk->top++] = i;
     }
 }
 /////////////////////////////单调栈模板/////////////////////////////
 
+MonotonicStack stk;
 type h[maxn];
 int l[maxn];
 int r[maxn];
@@ -53,9 +63,9 @@ int main() {
     for (int i = 1; i <= n; ++i) {
         scanf("%d", &h[i]);
     }
-    findFirstMeetOnLeft(n, h, l);
+    findFirstMeetOnLeft(&stk, n, h, l, cmpLT);
     reverseArray(n, h);
-    findFirstMeetOnLeft(n, h, r);
+    findFirstMeetOnLeft(&stk, n, h, r, cmpLT);
     reverseArray(n, h);
     reverseArray(n, r);
     for (int i = 1; i <= n; ++i) {
