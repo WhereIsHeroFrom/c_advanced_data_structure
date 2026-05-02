793cad53f79a0d56bd9afc0f6754f4f52085aee5
WhereIsHeroFrom <menjitianya2007@163.com>
2026-05-02 09:46:48 +0800

    模板化完成

--- 06、单调队列/6-3 刷题实战/6-3-2 单调队列/main.c
+++ 06、单调队列/6-3 刷题实战/6-3-2 单调队列/main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-#define maxn 100001
-
-int h[maxn], ans[maxn];
+/////////////////////////////单调队列模板/////////////////////////////
+#define type int
+#define maxn 1000001
+typedef int (* cmp_t)(type a, type b);
 
 typedef struct {
     int data[maxn];
@@ -11,44 +11,59 @@ typedef struct {
     int rear;
 } MonotonicQueue;
 
-MonotonicQueue deque;
-
-int cmp_max(int a, int b) {
+// <  单调递增队列(队首->队尾)：1 2 3 4 5
+int cmpLT(type a, type b) {
+    return a < b;
+}
+// <= 单调不减队列(队首->队尾)：2 2 3 3 4
+int cmpLE(type a, type b) {
     return a <= b;
 }
-
-int cmp_min(int a, int b) {
+// >  单调递减队列(队首->队尾)：6 5 4 3 2
+int cmpGT(type a, type b) {
+    return a > b;
+}
+// >= 单调不增队列(队首->队尾): 6 6 5 4 3
+int cmpGE(type a, type b) {
     return a >= b;
 }
 
-void findIntervalMinMax(int n, int k, int (*cmp)(int, int)) {
-    deque.front = 0;
-    deque.rear = 0;
-    for (int i = 0; i < n; ++i) {
-        while (deque.rear > deque.front && cmp(h[deque.data[deque.rear - 1]], h[i])) {
-            deque.rear--;
+// ans[i] 代表 [i-k+1, i] 中，最大值或最小值的下标
+// cmpGT :  存储最大值的下标
+// cmpLT :  存储最小值的下标
+void findIntervalMinMax(MonotonicQueue *que, 
+    int n, int k, type h[], int ans[], cmp_t cmp) {
+    que->front = que->rear = 0;
+    for (int i = 1; i <= n; ++i) {
+        while (que->rear > que->front && !cmp(h[que->data[que->rear - 1]], h[i])) {
+            que->rear--;
         }
-        deque.data[deque.rear++] = i;
-        while (deque.data[deque.rear - 1] - deque.data[deque.front] + 1 > k) {
-            deque.front++;
+        que->data[que->rear++] = i;
+        while (que->data[que->rear - 1] - que->data[que->front] + 1 > k) {
+            que->front++;
         }
-        ans[i] = h[deque.data[deque.front]];
+        ans[i] = h[que->data[que->front]];
     }
 }
+/////////////////////////////单调队列模板/////////////////////////////
+
+MonotonicQueue que;
+type h[maxn];
+int ans[maxn];
 
 int main() {
     int n, k;
     scanf("%d %d", &n, &k);
-    for (int i = 0; i < n; ++i) {
+    for (int i = 1; i <= n; ++i) {
         scanf("%d", &h[i]);
     }
-    findIntervalMinMax(n, k, cmp_min);
-    for (int i = k - 1; i < n; ++i) {
+    findIntervalMinMax(&que, n, k, h, ans, cmpLT);
+    for (int i = k; i <= n; ++i) {
         printf("%d ", ans[i]);
     }
     printf("\n");
-    findIntervalMinMax(n, k, cmp_max);
-    for (int i = k - 1; i < n; ++i) {
+    findIntervalMinMax(&que, n, k, h, ans, cmpGT);
+    for (int i = k; i <= n; ++i) {
         printf("%d ", ans[i]);
     }
     printf("\n");
--- 06、单调队列/6-3 刷题实战/6-3-3 MAX最差值/main.c
+++ 06、单调队列/6-3 刷题实战/6-3-3 MAX最差值/main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
 
+/////////////////////////////单调队列模板/////////////////////////////
+#define type int
 #define maxn 1000001
-
-int h[maxn], G[maxn], F[maxn];
+typedef int (* cmp_t)(type a, type b);
 
 typedef struct {
     int data[maxn];
@@ -11,41 +11,57 @@ typedef struct {
     int rear;
 } MonotonicQueue;
 
-MonotonicQueue deque;
-
-int cmp_max(int a, int b) {
+// <  单调递增队列(队首->队尾)：1 2 3 4 5
+int cmpLT(type a, type b) {
+    return a < b;
+}
+// <= 单调不减队列(队首->队尾)：2 2 3 3 4
+int cmpLE(type a, type b) {
     return a <= b;
 }
-
-int cmp_min(int a, int b) {
+// >  单调递减队列(队首->队尾)：6 5 4 3 2
+int cmpGT(type a, type b) {
+    return a > b;
+}
+// >= 单调不增队列(队首->队尾): 6 6 5 4 3
+int cmpGE(type a, type b) {
     return a >= b;
 }
 
-void findIntervalMinMax(int n, int k, int ans[], int (*cmp)(int, int)) {
-    deque.front = 0;
-    deque.rear = 0;
-    for (int i = 0; i < n; ++i) {
-        while (deque.rear > deque.front && cmp(h[deque.data[deque.rear - 1]], h[i])) {
-            deque.rear--;
+// ans[i] 代表 [i-k+1, i] 中，最大值或最小值的下标
+// cmpGT :  存储最大值的下标
+// cmpLT :  存储最小值的下标
+void findIntervalMinMax(MonotonicQueue *que, 
+    int n, int k, type h[], int ans[], cmp_t cmp) {
+    que->front = que->rear = 0;
+    for (int i = 1; i <= n; ++i) {
+        while (que->rear > que->front && !cmp(h[que->data[que->rear - 1]], h[i])) {
+            que->rear--;
         }
-        deque.data[deque.rear++] = i;
-        while (deque.data[deque.rear - 1] - deque.data[deque.front] + 1 > k) {
-            deque.front++;
+        que->data[que->rear++] = i;
+        while (que->data[que->rear - 1] - que->data[que->front] + 1 > k) {
+            que->front++;
         }
-        ans[i] = h[deque.data[deque.front]];
+        ans[i] = h[que->data[que->front]];
     }
 }
+/////////////////////////////单调队列模板/////////////////////////////
+
+MonotonicQueue que;
+type h[maxn];
+int G[maxn];
+int F[maxn];
 
 int main() {
     int n, k;
     scanf("%d %d", &n, &k);
-    for (int i = 0; i < n; ++i) {
+    for (int i = 1; i <= n; ++i) {
         scanf("%d", &h[i]);
     }
-    findIntervalMinMax(n, k, G, cmp_max);
-    findIntervalMinMax(n, k, F, cmp_min);
+    findIntervalMinMax(&que, n, k, h, G, cmpGT);
+    findIntervalMinMax(&que, n, k, h, F, cmpLT);
     int ret = -1000000000;
-    for (int i = 0; i < n; ++i) {
+    for (int i = 1; i <= n; ++i) {
         int v = G[i] - F[i];
         if (v > ret) {
             ret = v;
