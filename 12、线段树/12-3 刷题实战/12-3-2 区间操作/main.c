11901ac475dfecbbebcd5b2e1ce201404c9a3fad
WhereIsHeroFrom <menjitianya2007@163.com>
2026-05-07 10:11:53 +0800

    模板化完成

--- 12、线段树/12-3 刷题实战/12-3-2 区间操作/main.c
+++ 12、线段树/12-3 刷题实战/12-3-2 区间操作/main.c
@@ -1,101 +1,119 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/////////////////////////线段树模板(区间求和)/////////////////////////
+
+#define type long long
 #define maxn 100001
 
-long long tree[4 * maxn];
-long long lazy[4 * maxn];
-long long arr[maxn];
-int n;
+typedef struct {
+    type tree[4 * maxn];
+    type lazy[4 * maxn];
+    type arr[maxn];
+    int n;
+} SegmentTree;
 
-void build(int node, int start, int end) {
+void build(SegmentTree* st, int node, int start, int end) {
     if (start == end) {
-        tree[node] = arr[start];
+        st->tree[node] = st->arr[start];
         return;
     }
     int mid = (start + end) / 2;
     int leftChild = node * 2 + 1;
     int rightChild = node * 2 + 2;
-    build(leftChild, start, mid);
-    build(rightChild, mid + 1, end);
-    tree[node] = tree[leftChild] + tree[rightChild];
+    build(st, leftChild, start, mid);
+    build(st, rightChild, mid + 1, end);
+    st->tree[node] = st->tree[leftChild] + st->tree[rightChild];
 }
 
-void pushDown(int node, int start, int end) {
-    if (lazy[node] != 0) {
+void pushDown(SegmentTree* st, int node, int start, int end) {
+    if (st->lazy[node] != 0) {
         int mid = (start + end) / 2;
         int leftChild = node * 2 + 1;
         int rightChild = node * 2 + 2;
-        tree[leftChild] += lazy[node] * (mid - start + 1);
-        tree[rightChild] += lazy[node] * (end - mid);
-        lazy[leftChild] += lazy[node];
-        lazy[rightChild] += lazy[node];
-        lazy[node] = 0;
+        st->tree[leftChild] += st->lazy[node] * (mid - start + 1);
+        st->tree[rightChild] += st->lazy[node] * (end - mid);
+        st->lazy[leftChild] += st->lazy[node];
+        st->lazy[rightChild] += st->lazy[node];
+        st->lazy[node] = 0;
     }
 }
 
-void updateRange(int node, int start, int end, int l, int r, long long val) {
+void updateRange(SegmentTree* st, int node, int start, int end, int l, int r, type val) {
     if (start > r || end < l) return;
     if (start >= l && end <= r) {
-        tree[node] += val * (end - start + 1);
-        lazy[node] += val;
+        st->tree[node] += val * (end - start + 1);
+        st->lazy[node] += val;
         return;
     }
-    pushDown(node, start, end);
+    pushDown(st, node, start, end);
     int mid = (start + end) / 2;
     int leftChild = node * 2 + 1;
     int rightChild = node * 2 + 2;
-    updateRange(leftChild, start, mid, l, r, val);
-    updateRange(rightChild, mid + 1, end, l, r, val);
-    tree[node] = tree[leftChild] + tree[rightChild];
+    updateRange(st, leftChild, start, mid, l, r, val);
+    updateRange(st, rightChild, mid + 1, end, l, r, val);
+    st->tree[node] = st->tree[leftChild] + st->tree[rightChild];
 }
 
-long long queryRange(int node, int start, int end, int l, int r) {
+type queryRange(SegmentTree* st, int node, int start, int end, int l, int r) {
     if (start > r || end < l) return 0;
     if (start >= l && end <= r) {
-        return tree[node];
+        return st->tree[node];
     }
-    pushDown(node, start, end);
+    pushDown(st, node, start, end);
     int mid = (start + end) / 2;
     int leftChild = node * 2 + 1;
     int rightChild = node * 2 + 2;
-    long long leftSum = queryRange(leftChild, start, mid, l, r);
-    long long rightSum = queryRange(rightChild, mid + 1, end, l, r);
+    type leftSum = queryRange(st, leftChild, start, mid, l, r);
+    type rightSum = queryRange(st, rightChild, mid + 1, end, l, r);
     return leftSum + rightSum;
 }
 
-void SegmentTree_Init() {
-    build(0, 0, n - 1);
+void SegmentTree_Init(SegmentTree* st, int n, type* arr) {
+    st->n = n;
+    for (int i = 0; i < n; ++i) {
+        st->arr[i] = arr[i];
+    }
+    for (int i = 0; i < 4 * n; ++i) {
+        st->tree[i] = 0;
+        st->lazy[i] = 0;
+    }
+    build(st, 0, 0, n - 1);
 }
 
-void SegmentTree_Update(int l, int r, long long val) {
-    updateRange(0, 0, n - 1, l, r, val);
+void SegmentTree_Update(SegmentTree* st, int l, int r, type val) {
+    updateRange(st, 0, 0, st->n - 1, l, r, val);
 }
 
-long long SegmentTree_Query(int l, int r) {
-    return queryRange(0, 0, n - 1, l, r);
+type SegmentTree_Query(SegmentTree* st, int l, int r) {
+    return queryRange(st, 0, 0, st->n - 1, l, r);
 }
+/////////////////////////线段树模板(区间求和)/////////////////////////
+
+SegmentTree st;
+
 
 int main() {
-    int m;
+    int n, m;
     scanf("%d %d", &n, &m);
+    type arr[maxn];
     for (int i = 0; i < n; ++i) {
         scanf("%lld", &arr[i]);
     }
-    SegmentTree_Init();
+    SegmentTree_Init(&st, n, arr);
     while (m--) {
         int z, x, y, k;
         scanf("%d", &z);
         if (z == 1) {
             scanf("%d %d %d", &x, &y, &k);
-            x--;
-            y--;
-            SegmentTree_Update(x, y, k);
+            x -= 1;
+            y -= 1;
+            SegmentTree_Update(&st, x, y, k);
         } else {
             scanf("%d %d", &x, &y);
-            x--;
-            y--;
-            printf("%lld\n", SegmentTree_Query(x, y));
+            x -= 1;
+            y -= 1;
+            printf("%lld\n", SegmentTree_Query(&st, x, y));
         }
     }
     return 0;
